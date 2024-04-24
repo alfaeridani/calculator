dea63a0c278203995e2678a6a164919893af1a1e
alfaeridani <115158881+alfaeridani@users.noreply.github.com>
2024-04-24 23:08:43 +0700

    Design the UI and fix some bugs

--- calculator.js
+++ calculator.js
@@ -1,68 +1,71 @@
-function add(first, second) {
-    return Number(first) + Number(second);
-}
-
-function subtract(first, second) {
-    return Number(first) - Number(second);
-}
-
-function multiply(first, second) {
-    return Number(first) * Number(second);
-}
-
-function divide(first, second) {
-    return Number(first) / Number(second);
-}
-
 function operate(operator, first, second){
     switch (operator) {
         case '+':
-            result = add(first, second);
+            result = Number(first) + Number(second);
             break;
         case '-':
-            result = subtract(first, second);
+            result = Number(first) - Number(second);
             break;
         case '*':
-            result = multiply(first, second);
+            result = Number(first) * Number(second);
             break;
         case '/':
-            result = divide(first, second);
+            result = Number(first) / Number(second);;
             break;           
     }
     return result;
 }
 
+function checkDecimal(number) {
+    return number != parseInt(number);
+}
+
+function limitDecimal(number, maxDecimal) {
+    const roundedNumber = parseFloat(number.toFixed(maxDecimal));
+    return roundedNumber;
+}
+
 let display = document.querySelector(".display");
 let buttons = document.querySelectorAll("button");
-
 let displayedNumber = '';
 let storedNumber = '';
 let operator = '';
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        idName = button.id;
+        let idName = button.id;
         
-        if (idName.substring(0, 6) === 'number') {
+        if (idName.substring(0, 6) === 'number') { // Add number to the calc display
             number = button.id.charAt(6);
             displayedNumber += number;
             display.textContent = displayedNumber;
-        } else if (idName === 'clear') {
-            displayedNumber = displayedNumber.slice(0, -1);
+        } else if (idName === 'clear') { // Clear all values
+            displayedNumber = '';
+            storedNumber = '';
+            operator = '';
             display.textContent = displayedNumber;
-        } else if (idName === 'decimal' && displayedNumber.includes('.') === false) {
+        } else if (idName === 'decimal' && displayedNumber != '' && displayedNumber.includes('.') === false) { // Add decimal
             displayedNumber += '.';
             display.textContent = displayedNumber;
         } else if (idName === 'equals') {
-            result = operate(operator, storedNumber, displayedNumber);
-            displayedNumber = result;
-            display.textContent = displayedNumber;
-        } else {
+            if (storedNumber != '' && displayedNumber != '') {
+                result = operate(operator, storedNumber, displayedNumber);
+                if (checkDecimal(result) === true) {
+                    result = limitDecimal(result, 3)
+                }
+                storedNumber = result;
+                display.textContent = storedNumber;
+                displayedNumber = '';
+            }
+        } else if (idName === 'add' || idName === 'subtract' || idName === 'multiply' || idName === 'divide'){
             if (storedNumber === '') {
                 storedNumber = displayedNumber;
                 displayedNumber = '';
-            } else if (storedNumber != '') {
+            } else if (storedNumber != '' && displayedNumber != '') {
                 result = operate(operator, storedNumber, displayedNumber);
+                if (checkDecimal(result) === true) {
+                    result = limitDecimal(result, 3)
+                }
                 storedNumber = result;
                 display.textContent = storedNumber;
                 displayedNumber = '';
